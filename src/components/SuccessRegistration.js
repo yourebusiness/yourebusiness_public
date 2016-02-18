521063e54bc9029c5b4f380309f00c3612e7479e
Jhunexjun <Jhunexjun>
2016-02-18 19:33:51 +0800

    Beautify the response html.

--- src/components/SuccessRegistration.js
+++ src/components/SuccessRegistration.js
@@ -3,6 +3,10 @@ import React from 'react';
 
 export default React.createClass({
 	render: function() {
-		return (<div>Successful!!!</div>);
+		return (<div className="container">
+            <h4 className="page_title">Successful</h4>
+            	<p>Congratualtions!!! You have successfull registered.</p>
+            	<p>Please check your email for the registration code that you have to enter on your first login.</p>
+        	</div>);
 	}
 });
