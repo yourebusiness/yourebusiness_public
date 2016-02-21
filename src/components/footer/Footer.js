d1822a7eae9931feecfbc789fd6f06f10414fd61
Jhunexjun <Jhunexjun>
2016-02-21 16:54:14 +0800

    Enhance css.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,20 @@ export default React.createClass({
     render: function() {
         return (<div id="footer">
                     <div className="container">
-                        <p className="muted credit">Example courtesy <a href="http://martinbean.co.uk">Jhunex Jun</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+                    	<div className="row">
+			                <div className="col-sm-2">
+			                    <h6>Copyright &copy; 2015</h6>
+			                </div>
+			                <div className="col-sm-2">
+			                    <h6>Navigation</h6>
+			                    <ul className="list-unstyled">
+			                        <li><a href="#"><span className="glyphicon glyphicon-home"></span> Home</a></li>
+			                        <li><a href="#"><span className="glyphicon glyphicon-wrench"></span> Services</a></li>
+			                        <li><a href="http://yourspa.com/index.php/aboutus/view"><span className="glyphicon glyphicon-grain"></span> About Us</a></li>
+			                    </ul>
+			                </div>
+			                <div className="col-sm-8"></div>
+			            </div>
                     </div>
                 </div>);
     }
